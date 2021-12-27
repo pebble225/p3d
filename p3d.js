253ff8ae70eff045c527813f749df69db63aa7a4
Caleb Bradley <dev.pebble225@gmail.com>
2021-12-27 07:55:07 -0600

    Copied HSL2RGB code

--- p3d.js
+++ p3d.js
@@ -109,16 +109,84 @@ var MatrixUtils = {
 		{
 			console.log(m[4 * y + 0] + " " + m[4 * y + 1] + " " + m[4 * y + 2] + " " + m[4 * y + 3]);
 		}
-		console.log("");
 	}
 };
 
-function pushColor(meshArray, colorFunction)
+//
+
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   {Number}  r       The red color value
+ * @param   {Number}  g       The green color value
+ * @param   {Number}  b       The blue color value
+ * @return  {Array}           The HSL representation
+ */
+ function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {Number}  h       The hue
+ * @param   {Number}  s       The saturation
+ * @param   {Number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [r * 255, g * 255, b * 255];
+}
+
+function Material_HSL(h,s,l)
 {
-	for (var i = 0; i < colorFunction.length; i++)
-	{
-		meshArray.push(colorFunction[i]);
-	}
+	this.h = h;
+	this.s = s;
+	this.l = l;
 }
 
 function appendIndex(arr, element)
@@ -140,6 +208,7 @@ function RenderCache()
 }
 
 var global_meshDatabank = [];
+var global_materialDatabank = [];
 
 function MeshData(vertices, indices, normals, colors)
 {
@@ -161,6 +230,11 @@ function TriangleData(vertices, normals, color, mesh)//maybe it's a bad idea to 
 function appendMeshData(m)
 {
 	return appendIndex(global_meshDatabank, m);
+}
+
+function appendMaterial_HSV(m)
+{
+	return appendIndex(global_materialDatabank, m);
 }
 
 /**
@@ -276,6 +350,17 @@ function appendGameObject(rc, mesh, td, camera)
 			mesh
 		);
 	}
+}
+
+/**
+ * This function generates all of the draw information for the frame including the rendering order and list of 2d triangles
+ * @param {RenderCache} rc This is the render cache.
+ * @param {Array(16)} proj This is the perspective matrix that will be used to generate 3D. Generate and store this ahead of time.
+ */
+
+function prepareFrame(rc, proj)
+{
+
 }
 
 function TransformData()
